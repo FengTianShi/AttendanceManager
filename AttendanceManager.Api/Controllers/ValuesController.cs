7fb4b1a91027186c774499c0265aaec88acdd3a3
FengTianShi <fts2470216@icloud.com>
2019-07-29 00:36:59 +0900

    Update ValuesController.cs

--- AttendanceManager.Api/Controllers/ValuesController.cs
+++ AttendanceManager.Api/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace AttendanceManager.Api.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "hello", "from server" };
         }
 
         // GET api/values/5
