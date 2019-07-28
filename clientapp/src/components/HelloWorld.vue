9ccd4a742fecd2c6f1c19377fa608f49a9b7689e
FengTianShi <fts2470216@icloud.com>
2019-07-29 02:26:24 +0900

    Update HelloWorld.vue

--- clientapp/src/components/HelloWorld.vue
+++ clientapp/src/components/HelloWorld.vue
@@ -2,11 +2,6 @@
   <v-container>
     <v-layout text-center wrap>
       <v-flex xs12>
-        
-        <v-flex xs12 sm6 md3>
-          <v-text-field label="Regular"></v-text-field>
-        </v-flex>
-
         <v-img :src="require('../assets/logo.svg')" class="my-3" contain height="200"></v-img>
       </v-flex>
 
